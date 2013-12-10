a5384bb8eef44c792817b313abbc22af2bde6409
mart_s <mart_s@epitech.eu>
2013-12-10 17:27:05 +0100

    Initial

---
+++ setup.py
@@ -0,0 +1,30 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+
+company_name = "Epitech"
+product_name = "Blih"
+
+bdist_msi_options = {
+    'add_to_path': True,
+    'initial_target_dir': r'[ProgramFilesFolder]\%s\%s' % (company_name, product_name),
+    }
+
+build_exe_options = {
+	
+}
+
+base = 'Console'
+
+executables = [
+    Executable('blih.py', base=base)
+]
+
+setup(name='blih2exe',
+      version = '0.1',
+      description = 'Portage de blih sur windows',
+      options={
+          'bdist_msi': bdist_msi_options,
+          'build_exe': build_exe_options},
+      executables = executables)
